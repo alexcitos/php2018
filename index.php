92a60cef99658733415bd6eab7b715fb707db049
alexcitos19 <alitos.rope@gmail.com>
2018-09-05 14:38:17 -0500

    Antes de iniciar POO

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+include('jobs.php');
+$name = 'Alex Rodríguez P';
+$limitMonths = 2000;
+
+?>
 <!doctype html>
 <html lang="en">
 
@@ -21,7 +27,7 @@
         <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
       </div>
       <div class="col">
-        <h1><?php echo 'Alex Rodríguez P';?></h1>
+        <h1><?php echo $name;?></h1>
         <h2>PHP Developer</h2>
         <ul>
           <li>Mail: hector@mail.com</li>
@@ -45,36 +51,17 @@
         <div>
           <h3 class="border-bottom-gray" >Work Experience</h3>
           <ul>
-            <li class="work-position">
-              <h5>PHP Developer</h5>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sapiente sed pariatur sint exercitationem eos expedita eveniet veniam ullam, quia neque facilis dicta voluptatibus. Eveniet doloremque ipsum itaque obcaecati nihil.</p>
-              <strong>Achievements:</strong>
-              <ul>
-                <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
-                <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
-                <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
-              </ul>
-            </li>
-            <li class="work-position">
-                <h5>PHP Developer</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sapiente sed pariatur sint exercitationem eos expedita eveniet veniam ullam, quia neque facilis dicta voluptatibus. Eveniet doloremque ipsum itaque obcaecati nihil.</p>
-                <strong>Achievements:</strong>
-                <ul>
-                  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
-                  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
-                  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
-                </ul>
-              </li>
-              <li class="work-position">
-                  <h5>PHP Developer</h5>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sapiente sed pariatur sint exercitationem eos expedita eveniet veniam ullam, quia neque facilis dicta voluptatibus. Eveniet doloremque ipsum itaque obcaecati nihil.</p>
-                  <strong>Achievements:</strong>
-                  <ul>
-                    <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
-                    <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
-                    <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
-                  </ul>
-                </li>
+            <?php
+            $totalMonths = 0;
+            for($idx = 0;$idx < count($jobs); $idx++) {
+                $totalMonths += $jobs[$idx]['months'];
+
+              if($totalMonths > $limitMonths) {
+                break;
+              }
+              printJob($jobs[$idx]);
+            } 
+            ?>
           </ul>
         </div>
         <div>
---
+++ jobs.php
@@ -0,0 +1,52 @@
+<?php
+$jobs = [
+    [
+      'title' => 'PHP Developer',
+      'description' => 'This job is awesome!!',
+      'visible' => true,
+      'months' => 16
+    ],
+    [
+      'title' => 'Python Dev',
+      'visible' => false,
+      'months' => 14
+    ],
+    [
+      'title' => 'DevOps',
+      'visible' => true,
+      'months' => 5
+    ],
+    [
+      'title' => 'NodeJS Dev',
+      'visible' => true,
+      'months' => 24
+    ],
+    [
+      'title' => 'Frontend Dev',
+      'visible' => true,
+      'months' => 3
+    ]
+  ];
+  function getDuration($months) {
+    $years = floor($months / 12);
+    $extraMonths = $months % 12;
+    return "$years years $extraMonths months";
+  }
+  
+  function printJob($job) {
+    if($job['visible'] == false) {
+      return;
+    }
+  
+    echo '<li class="work-position">';
+    echo '<h5>' . $job['title'] . '</h5>';
+    echo '<p>' . $job['description'] . '</p>';
+    echo '<p>' . getDuration($job['months']) . '</p>';
+    echo '<strong>Achievements:</strong>';
+    echo '<ul>';
+    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+    echo '</ul>';
+    echo '</li>';
+  }  
